ecb97560d746a44eb787e5108d8b7182d6885bd7
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2019-10-28 15:05:37 +0100

    added

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,23 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Events.css'
-class Events extends Component {
-  render() {
-    return (
-        <div>
-            <h1 className="h1class">Events</h1>
-            <div className="eventContainer">
-                <div className="specificEvent">
-                    <p>
-                    test paragraph
-                    </p>
-                    <img alt="test">
-                    </img>
-                </div>
-            </div>
+
+export default function Events() {
+  return (
+    <div className="mainDiv">
+        <h3>WED, OCT 7, 7:00 PM</h3>
+        <div className="container">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis recusandae, inventore, officiis obcaecati assumenda veniam eius saepe esse provident eligendi animi magni natus illum optio. Earum ratione enim dolores. Nam mollitia, dolorem qui molestias, hic placeat, ducimus quibusdam ratione culpa a deserunt. Odio mollitia, recusandae maiores esse rem quo qui!
+
         </div>
-    );
+    </div>
+  );
 }
-}
-
-
-export default Events;
