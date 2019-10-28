00cb037537700b50e27433bbbcc81d8666eaf517
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2019-10-28 15:26:59 +0100

    test

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './Events.css'
+import styles from './Events.css'
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
 export default function Events() {
   return (
-    <div className="mainDiv">
+    <div className={styles.mainDiv}>
         <h3>WED, OCT 7, 7:00 PM</h3>
-        <div className="container">
+        <div className={styles.container}>
+          <LocationOnIcon></LocationOnIcon>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis recusandae, inventore, officiis obcaecati assumenda veniam eius saepe esse provident eligendi animi magni natus illum optio. Earum ratione enim dolores. Nam mollitia, dolorem qui molestias, hic placeat, ducimus quibusdam ratione culpa a deserunt. Odio mollitia, recusandae maiores esse rem quo qui!
 
         </div>
