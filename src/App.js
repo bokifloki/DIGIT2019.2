1d2068457623c03964879499bd39539a046b74da
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2019-10-28 15:35:34 +0100

    aa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import Donations from './components/Donations/Donations'
 import './App.css';
 
@@ -8,16 +7,6 @@ function App() {
     <div className="App">
      <Donations></Donations>
     </div>
-=======
-
-import SanoApp from './SanoApp'
-
-function App() {
-  return (
-    <>
-    <SanoApp />
-    </>
->>>>>>> 61d65216b847426427924e74ebcb6e53b02ac99a
   );
 }
 
