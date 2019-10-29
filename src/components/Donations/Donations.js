442598b9fbef64d4ea2dcb39eeb563d440694270
bokifloki <bbokilastname@yahoo.com>
2019-10-29 13:26:41 +0100

    npm i changes

--- src/components/Donations/Donations.js
+++ src/components/Donations/Donations.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./Donations.css";
+
 function Donations() {
   const donationAmounts = [2, 5, 10, 20, 25, 50, 100, 120];
   return (
     <div>
-      <div className="container">
-        <div className="body">
-          <div className="centerContainer">
+      <div className={styles.container}>
+        <div className={styles.body}>
+          <div className={styles.centerContainer}>
             <h1>DONATE TODAY</h1>
             <h3>Select an amount</h3>
             {donationAmounts.map((donation, index) => (
