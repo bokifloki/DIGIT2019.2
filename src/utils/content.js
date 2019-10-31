9e8597ed1421347fffc50bec4ff4427587a03fcc
MarioKrstevski <mariokrstevski@hotmail.com>
2019-10-31 21:45:34 +0100

    UPD [content] Macedonian versions added of first two paragraphs

--- src/utils/content.js
+++ src/utils/content.js
@@ -4,84 +4,169 @@ import { twitter } from "react-icons-kit/ikons/twitter";
 import { apple } from "react-icons-kit/ikons/apple";
 
 export default {
-  aboutus: {
-    donationsAmount: {
-      heading: "100% of your money brings resources to kids in need"
+  mk: {
+    aboutus: {
+      donationsAmount: {
+        heading: "100% of your money brings resources to kids in need"
+      }
+    },
+    homepage: {
+      testimonialContent: {
+        testimonials: [
+          {
+            icon: picture,
+            color: "red",
+            heading:
+              "In Africa alone, women spend 40 billion hours a year walking for water.",
+            quote:
+              "Access to clean water gives communities more time to grow food, earn an income, and go to school -- all of which fight poverty.",
+            person: "List [Member]",
+            title: "Time"
+          },
+          {
+            icon: microsoft,
+            color: "blue",
+            heading:
+              "Diseases from dirty water kill more people every year than all forms of violence, including war.",
+            quote:
+              "43% of those deaths are children under five years old. Access to clean water and basic sanitation can save around 16,000 lives every week.",
+            person: "Maria [Volunteer]",
+            title: "Health"
+          },
+          {
+            icon: twitter,
+            color: "green",
+            heading:
+              "Women are responsible for 72% of the water collected in Sub-Saharan Africa.",
+            quote:
+              "When a community gets water, women and girls get their lives back. They start businesses, improve their homes, and take charge of their own futures.",
+            person: "Bill [Member]",
+            title: "Empowerment"
+          },
+          {
+            icon: apple,
+            color: "yellow",
+            heading: "Clean water helps keep kids in school, especially girls.",
+            quote:
+              "Less time collecting water means more time in class. Clean water and proper toilets at school means teenage girls don’t have to stay home for a week out of every month.",
+            person: "George [Volunteer]",
+            title: "Education"
+          }
+        ],
+        heading: "",
+        sprite:
+          "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/brush-mars-cec3617ae7bd18a8baf716974ea51ca2.png",
+        h1: "Your can have a huge impact",
+        buttonText: "Learn More"
+      },
+      statisticInformation: {
+        sprite:
+          "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/brush-saturn-5a8595e0d8f85155761f87bf3d8ef574.png",
+        h1: `663 милиони луѓе низ светот живеат без чиста вода.`,
+        p1: ` Тоа се речиси 1 во 10 луѓе низ светот, или два пати популацијата на 
+        Америка. Повеќето живеат во изолирани рурални населби и го поминуваат по неколку саати дневно одејќи за да соберат вода за својата фамилија.
+        Не само што пешачењето ги им одзема од можноста да одат на школо или можноста нивните родители да спечалат пари, четопати и опасноста од
+        заразни болести е присутна која моќе сите да ги разболи.`,
+        p2: `Пристапноста до чиста вода значи едукација, заработка и здравје / посебно за жените и децата.`
+      },
+      welcomeContent: {
+        h1: "Зошто водата?",
+        p:
+          "За луѓето во земјите во разбој, чистата вода може да им смени се.",
+        buttonText: "Погледнете го видеото"
+      },
+      resources: {
+        images: {
+          illustration:
+            "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/hero-large-27870530f60dba9eb6f828d938dda6f1.jpg",
+          people:
+            "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/water-crisis-large-048145ec31eeb4be77749ea35680b6d3.jpg"
+        }
+      }
     }
   },
-  homepage: {
-    testimonialContent: {
-      testimonials: [
-        {
-          icon: picture,
-          color: "red",
-          heading:
-            "In Africa alone, women spend 40 billion hours a year walking for water.",
-          quote:
-            "Access to clean water gives communities more time to grow food, earn an income, and go to school -- all of which fight poverty.",
-          person: "List [Member]",
-          title: "Time"
-        },
-        {
-          icon: microsoft,
-          color: "blue",
-          heading:
-            "Diseases from dirty water kill more people every year than all forms of violence, including war.",
-          quote:
-            "43% of those deaths are children under five years old. Access to clean water and basic sanitation can save around 16,000 lives every week.",
-          person: "Maria [Volunteer]",
-          title: "Health"
-        },
-        {
-          icon: twitter,
-          color: "green",
-          heading:
-            "Women are responsible for 72% of the water collected in Sub-Saharan Africa.",
-          quote:
-            "When a community gets water, women and girls get their lives back. They start businesses, improve their homes, and take charge of their own futures.",
-          person: "Bill [Member]",
-          title: "Empowerment"
-        },
-        {
-          icon: apple,
-          color: "yellow",
-          heading: "Clean water helps keep kids in school, especially girls.",
-          quote:
-            "Less time collecting water means more time in class. Clean water and proper toilets at school means teenage girls don’t have to stay home for a week out of every month.",
-          person: "George [Volunteer]",
-          title: "Education"
-        }
-      ],
-      heading: "",
-      sprite:
-        "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/brush-mars-cec3617ae7bd18a8baf716974ea51ca2.png",
-      h1: "Your can have a huge impact",
-      buttonText: "Learn More"
+  eng: {
+    aboutus: {
+      donationsAmount: {
+        heading: "100% of your money brings resources to kids in need"
+      }
     },
-    statisticInformation: {
-      sprite:
-        "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/brush-saturn-5a8595e0d8f85155761f87bf3d8ef574.png",
-      h1: "663 million people in the world live without clean water",
-      p1: `That’s nearly 1 in 10 people worldwide. Or, twice the population
-        of the United States. The majority live in isolated rural areas
-        and spend hours every day walking to collect water for their
-        family. Not only does walking for water keep children out of
-        school or take up time that parents could be using to earn money,
-        but the water often carries diseases that can make everyone sick.`,
-      p2: ` But access to clean water means education, income and health -
-        especially for women and kids.`
-    },
-    welcomeContent: {
-      h1: "Why Water?",
-      p:
-        "For people in developing countries, clean water can change everything.",
-      buttonText: "Watch our video"
-    },
-    resources: {
+    homepage: {
+      testimonialContent: {
+        testimonials: [
+          {
+            icon: picture,
+            color: "red",
+            heading:
+              "In Africa alone, women spend 40 billion hours a year walking for water.",
+            quote:
+              "Access to clean water gives communities more time to grow food, earn an income, and go to school -- all of which fight poverty.",
+            person: "List [Member]",
+            title: "Time"
+          },
+          {
+            icon: microsoft,
+            color: "blue",
+            heading:
+              "Diseases from dirty water kill more people every year than all forms of violence, including war.",
+            quote:
+              "43% of those deaths are children under five years old. Access to clean water and basic sanitation can save around 16,000 lives every week.",
+            person: "Maria [Volunteer]",
+            title: "Health"
+          },
+          {
+            icon: twitter,
+            color: "green",
+            heading:
+              "Women are responsible for 72% of the water collected in Sub-Saharan Africa.",
+            quote:
+              "When a community gets water, women and girls get their lives back. They start businesses, improve their homes, and take charge of their own futures.",
+            person: "Bill [Member]",
+            title: "Empowerment"
+          },
+          {
+            icon: apple,
+            color: "yellow",
+            heading: "Clean water helps keep kids in school, especially girls.",
+            quote:
+              "Less time collecting water means more time in class. Clean water and proper toilets at school means teenage girls don’t have to stay home for a week out of every month.",
+            person: "George [Volunteer]",
+            title: "Education"
+          }
+        ],
+        heading: "",
+        sprite:
+          "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/brush-mars-cec3617ae7bd18a8baf716974ea51ca2.png",
+        h1: "Your can have a huge impact",
+        buttonText: "Learn More"
+      },
+      statisticInformation: {
+        sprite:
+          "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/brush-saturn-5a8595e0d8f85155761f87bf3d8ef574.png",
+        h1: "663 million people in the world live without clean water",
+        p1: `That’s nearly 1 in 10 people worldwide. Or, twice the population
+          of the United States. The majority live in isolated rural areas
+          and spend hours every day walking to collect water for their
+          family. Not only does walking for water keep children out of
+          school or take up time that parents could be using to earn money,
+          but the water often carries diseases that can make everyone sick.`,
+        p2: ` But access to clean water means education, income and health -
+          especially for women and kids.`
+      },
+      welcomeContent: {
+        h1: "Why Water?",
+        p:
+          "For people in developing countries, clean water can change everything.",
+        buttonText: "Watch our video"
+      },
+      resources: {
         images: {
-            illustration:"https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/hero-large-27870530f60dba9eb6f828d938dda6f1.jpg",
-            people:"https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/water-crisis-large-048145ec31eeb4be77749ea35680b6d3.jpg",
+          illustration:
+            "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/hero-large-27870530f60dba9eb6f828d938dda6f1.jpg",
+          people:
+            "https://d26p6gt0m19hor.cloudfront.net/assets/static-pages/whywater/water-crisis-large-048145ec31eeb4be77749ea35680b6d3.jpg"
         }
+      }
     }
   }
 };
