5174be99d7a05d5f70fdbaeb0c623a50af367859
Ivan Jakimovski <ivan.jakim1004@gmail.com>
2019-11-01 14:12:34 +0100

    a

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,5 +1,5 @@
-import React,{useState, useContext} from "react";
-import content from '../../utils/content'
+import React, { useState, useContext } from "react";
+import content from "../../utils/content";
 import Hero from "../Hero/Hero";
 import Map from "../Map/Map";
 import HeroSplit from "../HeroSplit/HeroSplit";
@@ -7,36 +7,37 @@ import WelcomeContent from "./components/WelcomeContent";
 import StatisticInformation from "./components/StatisticInformation";
 import TestimonialContent from "./components/TestimonialContent";
 
-import {LanguageContext} from '../../utils/LanguageContext'
+import { LanguageContext } from "../../utils/LanguageContext";
 
 const Homepage = () => {
+  const { language } = useContext(LanguageContext);
 
-  const {language}= useContext(LanguageContext)
+  const homepageContent = content[language].homepage;
 
-  const homepageContent = content[language].homepage
-
-  const {testimonialContent, statisticInformation, welcomeContent,} = homepageContent
-  const {illustration, people} = homepageContent.resources.images
+  const {
+    testimonialContent,
+    statisticInformation,
+    welcomeContent
+  } = homepageContent;
+  const { illustration, people } = homepageContent.resources.images;
 
   return (
     <>
-      <Hero img={illustration} cHeight={600} content={<WelcomeContent content={welcomeContent} />}/>
-      <HeroSplit 
-      lImg="https://cdn.pixabay.com/photo/2016/08/06/14/11/money-1574450_960_720.png"
-      lImgSize="contain"
-      rBgColor="transparent"
-      rightContent={<h1>Hello</h1>}
+      <Hero
+        img={illustration}
+        cHeight={600}
+        content={<WelcomeContent content={welcomeContent} />}
       />
       <Hero
         cHeight={500}
         linearGrad={"linear-gradient(#ebead6,#a8b9b5)"}
         content={<StatisticInformation content={statisticInformation} />}
       />
-      <Hero cHeight={700} img={people}/>
+      <Hero cHeight={700} img={people} />
       <Hero
         cHeight={996}
         linearGrad={"linear-gradient(#c9a77e,#c3e0dc)"}
-        content={<TestimonialContent  content={testimonialContent} />}
+        content={<TestimonialContent content={testimonialContent} />}
       />
       <Map />
     </>
